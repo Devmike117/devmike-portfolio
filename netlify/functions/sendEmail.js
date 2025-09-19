955b24334433022c0c2d319c1462f2c3c04e8dc2
Mike <mikealesso768@gmail.com>
2025-09-18 22:51:53 -0600

    add public key and private key

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -4,7 +4,6 @@ exports.handler = async function(event) {
   try {
     const data = JSON.parse(event.body);
 
-
     if (!data.from_name || !data.from_email || !data.message) {
       return {
         statusCode: 400,
@@ -12,19 +11,19 @@ exports.handler = async function(event) {
       };
     }
 
-
     const response = await fetch("https://api.emailjs.com/api/v1.0/email/send", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         service_id: process.env.EMAILJS_SERVICE_ID,
         template_id: process.env.EMAILJS_TEMPLATE_ID,
-        accessToken: process.env.EMAILJS_PRIVATE_KEY,
         template_params: {
           from_name: data.from_name,
           from_email: data.from_email,
           message: data.message
-        }
+        },
+        user_id: process.env.EMAILJS_PUBLIC_KEY,    
+        accessToken: process.env.EMAILJS_PRIVATE_KEY 
       })
     });
 
