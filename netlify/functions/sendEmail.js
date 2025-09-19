ea6ca1485b85c7debb4594029c7e960dd3c820ea
Mike <mikealesso768@gmail.com>
2025-09-18 22:19:09 -0600

    chore: update package.json and sendEmail function to use emailjs-com

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -1,18 +1,18 @@
-const emailjs = require("emailjs/browser");
+const emailjs = require("emailjs-com");
 
 exports.handler = async function (event) {
   const data = JSON.parse(event.body);
 
   try {
     const response = await emailjs.send(
-      process.env.EMAILJS_SERVICE_ID,
-      process.env.EMAILJS_TEMPLATE_ID,
+      process.env.EMAILJS_SERVICE_ID,   
+      process.env.EMAILJS_TEMPLATE_ID, 
       {
         from_name: data.from_name,
         from_email: data.from_email,
         message: data.message,
       },
-      process.env.EMAILJS_PUBLIC_KEY // 👉 ahora solo se usa la public key
+      process.env.EMAILJS_PUBLIC_KEY   
     );
 
     return {
